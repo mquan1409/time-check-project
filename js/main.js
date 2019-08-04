63084eef445080d972416f41374316a373eef4f9
Quan <minhquan14092002@gmail.com>
2019-08-05 02:37:02 +0700

    write item-content and delete item

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
-function addWriteButtonEvent(writeButton){
-    writeButton.addEventListener("click",(e)=>{
-        console.log("click");
-    });
+function addBtnWriteEvent(){
+    $(".item-content").hide();
+    $(".btn-write").click(function(){
+        $(this).parent().find(".item-display").hide()
+        $(this).parent().find(".item-content").show();
+        $(".item-content").focusout(function(){
+            $(".item-content").hide();
+            $("span:hidden").html($(this).val());
+            $(".item-display").show();
+        })
+    })
+    $(".btn-write").hide();
+    $("li").mouseenter(function(){
+        console.log("k");
+        $(this).find("div").show();
+    })
+    // $("li").mouseout(function(){
+    //     console.log("k");
+    //     $(this).find("div").hide();
+    // })
+}
+
+function addBtnDeleteItemEvent(){
+    $(".btn-delete-item").click(function(){
+        $(this).parent().remove();
+    })    
 }
 
 function addItemButtonEvent(addItemButton){
     addItemButton.addEventListener("click",(e)=>{
         var newItem = `
-            <li>+
+            <li>
+                <span class="item-display">+ </span>
+                <textarea class="item-content" name="" id="" cols="30" rows="10"></textarea>
                 <div class="btn-write">w</div>
+                <div class="btn-delete-item"></div>
             </li>
         `;
+        $(".btn-write").unbind();
         e.target.parentNode.getElementsByClassName("container")[0].insertAdjacentHTML("beforeend",newItem);
+        addBtnWriteEvent();
+        addBtnDeleteItemEvent();
         
-        var writeButtons = document.getElementsByClassName("btn-write");
-
     })
 }
 
@@ -31,7 +57,7 @@ addContainer.addEventListener("click",function(e){
     colNumber++;
     var newCol = `
         <div id="col-${colNumber}" class="col">
-            <ul id="${colNumber}" class="container">
+            <ul id="container-${colNumber}" class="container">
                 container-${colNumber}
             </ul>
             <div id="btn-add-item-${colNumber}" class="btn-add-item">Add</div>
@@ -47,12 +73,9 @@ var addItemButtons = document.getElementsByClassName("btn-add-item");
 for(var i=0;i<addItemButtons.length;i++){
     addItemButtonEvent(addItemButtons[i]);
 }
+addBtnWriteEvent();
 
-var writeButtons = document.getElementsByClassName("btn-write");
-
-for(var i=0;i<writeButtons.length;i++){
-    addWriteButtonEvent(writeButtons[i]);
-}
+addBtnDeleteItemEvent();
 
 
 
