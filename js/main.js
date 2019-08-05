cf44d87617cca6cd7a0438998cb0e7f46142150e
Quan <minhquan14092002@gmail.com>
2019-08-05 17:33:45 +0700

    add-item add-col delete-col delete-item

--- js/main.js
+++ js/main.js
@@ -3,21 +3,24 @@ function addBtnWriteEvent(){
     $(".btn-write").click(function(){
         $(this).parent().find(".item-display").hide()
         $(this).parent().find(".item-content").show();
+        $(this).parent().find(".item-content").focus();
         $(".item-content").focusout(function(){
+            if($(this).parent().find(".item-content").val()==""){
+                $(this).parent().remove();
+            }
             $(".item-content").hide();
             $("span:hidden").html($(this).val());
             $(".item-display").show();
-        })
+        });
     })
     $(".btn-write").hide();
-    $("li").mouseenter(function(){
-        console.log("k");
-        $(this).find("div").show();
-    })
-    // $("li").mouseout(function(){
-    //     console.log("k");
-    //     $(this).find("div").hide();
-    // })
+    $("li").hover(
+        function(){
+        $(this).find(".btn-write").show();
+    },
+    function(){
+        $(this).find(".btn-write").hide();
+    });
 }
 
 function addBtnDeleteItemEvent(){
@@ -26,8 +29,8 @@ function addBtnDeleteItemEvent(){
     })    
 }
 
-function addItemButtonEvent(addItemButton){
-    addItemButton.addEventListener("click",(e)=>{
+function addItemButtonEvent(){
+    $(".btn-add-item").click(function(e){
         var newItem = `
             <li>
                 <span class="item-display">+ </span>
@@ -40,7 +43,15 @@ function addItemButtonEvent(addItemButton){
         e.target.parentNode.getElementsByClassName("container")[0].insertAdjacentHTML("beforeend",newItem);
         addBtnWriteEvent();
         addBtnDeleteItemEvent();
-        
+        console.log($(this).siblings().find(".btn-write").last());
+        $(this).siblings().find(".btn-write").last().trigger("click");
+    });
+}
+
+function addBtnDeleteColEvent(){
+    $(".btn-delete-col").click(function(){
+        $(this).parent().remove();
+        // containers.containers.splice(colNumberInArray,1);
     })
 }
 
@@ -48,34 +59,46 @@ const containers = dragula([...document.getElementsByClassName("container")],{
     revertOnSpill:true
 });
 console.log(containers);
-// containers.removeOnSpill = true;
 
 var addContainer = document.getElementById("add-container");
 var colNumber = 2;
 
-addContainer.addEventListener("click",function(e){
+$("#add-container").click(function(e){
     colNumber++;
     var newCol = `
         <div id="col-${colNumber}" class="col">
-            <ul id="container-${colNumber}" class="container">
-                container-${colNumber}
-            </ul>
-            <div id="btn-add-item-${colNumber}" class="btn-add-item">Add</div>
+            <div class="col-content">
+                <div class="container-heading" contenteditable="true" style="outline:0px;"></div>
+                <ul id="container-${colNumber}" class="container">
+                </ul>
+                <div id="btn-add-item-${colNumber}" class="btn-add-item">Add</div>
+            </div>
+            <div class="btn-delete-col"></div>
         </div>
     `
-    document.getElementById("containers").insertAdjacentHTML("beforeend",newCol);
-    containers.containers.push(document.getElementById(`container-${colNumber}`))
-    addItemButtonEvent(document.getElementsByClassName("btn-add-item")[colNumber-1]);
+    $(".btn-delete-col").unbind();
+    $(".btn-add-item").unbind();
+    $(this).before(newCol);
+    containers.containers.push(document.getElementById(`container-${colNumber}`));
+    addItemButtonEvent();
+    addBtnDeleteColEvent();
+    $(".container-heading").last().focus();
+    // console.log($(".container-heading").last());
+    $(".container-heading").last().focusout(function(){
+        if($(".container-heading").last().text()==""){
+            $(".container-heading").last().parent().parent().remove();
+        }
+    });
 });
 
-var addItemButtons = document.getElementsByClassName("btn-add-item");
 
-for(var i=0;i<addItemButtons.length;i++){
-    addItemButtonEvent(addItemButtons[i]);
-}
+addItemButtonEvent();
+
 addBtnWriteEvent();
 
 addBtnDeleteItemEvent();
+
+addBtnDeleteColEvent();
 
 
 
