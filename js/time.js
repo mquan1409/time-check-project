6570beb39f530b5a1250aff86146215e8d454554
Quan <minhquan14092002@gmail.com>
2019-08-29 18:18:22 +0700

    font and
    nearly demo

--- js/time.js
+++ js/time.js
@@ -1,5 +1,21 @@
-var d = new Date();
-console.log(d.getHours());
+var newD = new Date();
+var day = newD.getDay();
+var month = newD.getMonth() + 1;
+var hour = newD.getHours();
+var min = newD.getMinutes();
+
+var date = new Date($('.date-input').val());
+    console.log(date);
+    console.log(date.getDate());
+    $(".item-duedate-content").hide();
+
+
+// for(var i=0; i<$(".date-input").length;i++){
+//     if($(".hour-input")[i].val())
+// }
+
+console.log($(".date-input"));
+
 
 function itemContentTasksEvent(){
     $(".item-duedate").unbind();
@@ -13,9 +29,16 @@ itemContentTasksEvent();
 
 
 
-$("#sub").click(function(){
-    var date = new Date($('.date-input').val());
-    console.log(date);
-    console.log(date.getDate());
-    $(".item-duedate-content").hide();
-});
+// $("#sub").click(function(){
+//     var date = new Date($('.date-input').val());
+//     console.log(date);
+//     console.log(date.getDate());
+//     $(".item-duedate-content").hide();
+// });
+// var y = 0;
+// while (y<5){
+//     for(var i=0; i<$(".date-input").length;i++){
+//        console.log($(".hour-input")[i].val);
+//     }
+//     y++;
+// }
